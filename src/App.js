18998888217dabb27dcc48938ad1c4a5e7171195
DESKTOP-1B4SU0S\Nikita <nikkofann@gmail.com>
2021-01-06 18:13:05 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import "./App.scss";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const App = () => {
-  useEffect(() => {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = "https://api.bizzabo.com/api/events";
+  const [value, onChange] = useState(new Date());
+  const [events, setEvents] = useState([]);
+  useEffect(() => {}, []);
+
+  const handleClick = () => {
     axios
-      .get("https://api.bizzabo.com/api/events", {
+      .get(proxyurl + url, {
         headers: {
           Authorization: `Bearer b2f9b657-d8fd-4c34-a28b-eba13cab25c2`,
         },
       })
-      .then((res) => console.log(res));
-  }, []);
-  return <div className="App">HEY</div>;
+      .then((res) => setEvents(res.data.content))
+      .catch((e) => console.log(e));
+
+    const date = new Date();
+    // const formattedDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+    console.log(date.toISOString());
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>PULL DATA</button>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        tileContent={({ activeStartDate, date, view }) => {
+          if (date.getDate() === 5) {
+            return <p>ITS YOUR DAY FUCKER</p>;
+          } else if (date.getDate() === 6) {
+            return <p>ITS NOT YOUR DAY</p>;
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
